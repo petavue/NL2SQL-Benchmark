7e1fbba868bbc59c5dd264904201a6040f1f7092
Siddarth-Petavue <sid@petavue.com>
2024-02-09 00:58:55 +0530

    self-hosting benchmark setup

--- set_env_vars.py
+++ set_env_vars.py
@@ -5,8 +5,10 @@ ANY_SCALE_API_KEY_PERSONAL = "esecret_***************"
 PETAVUE_OPENAI_KEY = "sk-**********************"
 AWS_ACCESS_KEY_ID = "*************"
 AWS_SECRET_ACCESS_KEY = "*******************************"
+HUGGING_FACE_TOKEN="hf_***************"
 
 # Set environment variables
-os.environ['ANY_SCALE_API_KEY'] = ANY_SCALE_API_KEY_PERSONAL
+os.environ['ANY_SCALE_API_KEY'] = ANY_SCALE_API_KEY_PETAVUE
 os.environ['AWS_ACCESS_KEY_ID'] = AWS_ACCESS_KEY_ID
 os.environ['AWS_SECRET_ACCESS_KEY'] = AWS_SECRET_ACCESS_KEY
+os.environ['HUGGING_FACE_TOKEN'] = HUGGING_FACE_TOKEN
